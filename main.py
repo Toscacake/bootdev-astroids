821df077f1a69f7978b7bd233acd416064ca30c5
Tobias Campbell <tobca235@student.liu.se>
2026-06-25 17:15:34 +0200

    Initial start of Astroids game, basic screen works

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+                
+            screen.fill("black")
+            pygame.display.flip()
+            pass
+        
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}\n Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
