e52d3139acfb0fe01cc1f5fc261401c40dfd7071
Tobias Campbell <tobca235@student.liu.se>
2026-06-26 21:23:02 +0200

    Floating astroid shapes implemented

--- main.py
+++ main.py
@@ -2,24 +2,44 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_RADIUS
 from player import Player
 from logger import log_state
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
+# INITIERING AV VARIABLER
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
+    Asteroid.containers = (asteroids, updatable, drawable)
+    #asteroids = Asteroid()
+
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
+# GAME-LOOP, KÖRS KONTINUELIGT
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-        dt = clock.tick(60) / 1000
-        player.update(dt)
+        # Fyll skärm
         screen.fill("black")
-        player.draw(screen)
+
+        # Uppdatera objekt i upd/draw -grupper
+        dt = clock.tick(60) / 1000
+        for thing in updatable:
+            thing.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+
         pygame.display.flip()
 
         
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
+import pygame
 from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
 from circleshape import CircleShape
-import pygame
 
 class Player(CircleShape):
     def __init__(self, x, y):
